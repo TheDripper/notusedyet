07aaf8b9da7dd0dc007b087b491081bfa0f77ff0
TheDripper <tylerehill@gmail.com>
2020-07-27 02:05:12 -0700

    not used yet

---
+++ pages/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="container">
+    <!-- <div v-for="(word, index) in words">
+      <p :key="index">{{ word }}</p>
+    </div>-->
+    <div class="container flex flex-col items-center mx-auto p-6">
+      <div>
+        <p>As I write code for $$$ in the VC-funded horse-race that is the Tech Industry in 2020, I've been reflecting on one quiet but radical protestor who is out in the parkling lot handing out free copies of the good book: WordPress.</p>
+      </div>
+      <div>
+        <p>When WordPress began in 2003, The Internet was still largely uncolonized and inhabited by nerds. Back then, the WordPress mission  <strong>to create a free and open source publishing platform for The Internet</strong> seemed like esoteric gibberish to anybody who wasn't actively publishing or reading content, i.e. not that many people when compared to our current COVID era, where the maintenance of our physical bodies is the last thing that hasn't yet been abstracted to an online software layer. I pray that this last citadel of sweat and poop and hair remains ours forever, amen.</p>
+      </div>
+      <div>
+        <p>As usual, we're lucky the nerds had a head-start before business moved in. If begun just this year, in today's economic climate, the WP mission would be considered quixotic or maybe even terrorism. This is tragic because it is this very potential to share new information at no cost that inspired the real heroes who created all this in the first place:</p>
+        <span class="flex">
+        <img src="/douglace.png" />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/M5PgQS3ZBWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </span>
+        <span class="flex items-start">
+          <img src="kay_bio.png" />
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/6ZdxiQoOBgs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <img id="moon" src="moon.png" />
+        </span>
+        <p class="mt-6">So a network of terminals and interface hardware that can abstract information into a visual space that's easily understood by children? Miraculous! And in '03, software engineers giving away free code to help eliminate the barrier-to-entry for the WWW? Our future is so bright!</p>
+        <h1 class="blink">--:--:--</h1>
+        <p>Act 2: Enter The Business Schools of America, and their hordes of young men who want to make a lot of money really fast.</p>
+        <p>These men are starving after the '08 crash cut short the Housing/Derivatives/CDO casino. They are twitching and stuck in a stagnant economy with nothing but slow, stable bonds. Suddenly they whiff the unmistakable lemon-fresh scent of fast money being made in large quantities. It seems to be coming from the Bay Area.</p>
+        <p>Here's the plan:</p>
+        <ol>
+          <li>Pick a basic feature of any society, e.g. 
+            <ul>
+              <li>a board in the town square to post announcements</li>
+              <li>an open marketplace for vendors to sell</li>
+            </ul>
+          <li>Abstract these fundamental concepts of human culture that are thousands of years old into an online software layer with programming languages you inherited for free from the prior scientific research of the US GOVT. by writing some basic functions to handle trivial, age-old Comp Sci / Web Dev concerns like:</li>
+            <ul>
+              <li>Posting an image to a server</li>
+              <li>Posting a string of text to a chat</li>
+              <li>Processing a credit card transaction</li>
+            </ul>
+          <li>Copywrite this curation of public work as "software" that you have "designed"</li>
+          <li>Use this as legal justification to collect a tariff on every human heartbeat while you scroll instagram for things to spend your obscene wealth on.</li>
+          </ol>
+        <p>There is no genius here, just young white men cashing in on the basic movements of life itself.</p>
+        <p>Behold: a band of wild investors, drunk with bloodthirst for their next unicorn, slaps a SaaS padlock on progress and begins an amphetaminic hype ritual with bright colors and cute cartoon characters and a whole quasi-magical language of metaphor and imagery to obfuscate the fact that they’re just reselling a spreadsheet, or some scripts to pipe data from Vendor A to Vendor B. These technologies were invented in the 80s, but only recently has Wall Street figured out that you can box these things up with bright branding, deploy them to the Bezos-net, and charge VC-funded companies $1000/month "for the service". 
+          <img src="saas.png" />
+          Millions of VC dollars later, 500-1000 people are hard at work on a product that has no reason to exist, except to extract illusory wealth out of other VC funded horses. Nobody's really paying taxes on any of this, because jobs ok? Don't make the angry rich boys cripple your state. This dowry bargaining between Silicon Valley and Capitol Hill (i.e. press statements about "incentive" and "pulling jobs out") is now carried out on the front page of NYT, proving that:</p>
+        <ul class="mt-8">
+         <li>It's not bribery if you say a lot of words first.</li>
+         <li>Once their ego has been threatened, Billionare Owners think nothing of sending thousands of Americans home unemployed.</li>
+         <li>This can be published in the newspaper because it is business as usual.</li>
+        </ul>
+        <img id="bribe" src="/bribe.png" class="mb-8" />
+        <p>Zuckerberg set the precedent for this, and it’s now the philosophy of the Bay Area and the greater American Economy: put a lock on everything and start building your <strong>passive</strong> income.  This money is defined by having been obtained without doing any work. Young men in San Francisco recite their monthly sums like prayer. Ask yourself what kind of country we become when this is the goal we all strive for, where energetic youngsters with no real skills are entiled to vast sums of money for having the “vision” or "courage" to horde and plagiarize the basic inventions of their scientific predecessors and sell out every pixel of our minds with hypnotic imagery to make us feel hungry and sad. 
+        <div class="flex mb-6">
+          <img src="/bernays.png" />
+          <img src="/honestly.png" />
+        </div>
+        <p>Methods and phraseology devised 100 years ago are still being uttered today, verbatim, in marketing meetings everywhere: Ulitmate, Family, Sexy, Delicious, Dopamine, Oxytocin, Adrenaline, etc.</p>
+        <p>The next time you see a bogus rich kid like Bezos or Zukerberg or Musk pretending to be a genius, remember that all their wealth comes from selling ads that cause depression and then kissing the hands of facists who are right now, in 2020, operating concentration camps.</p>
+        <div class="flex flex-col items-start">
+          <img src="concentration_camps.png" />
+          <img src="xi.png" />
+          <img src="sandberg.png" />
+          </div>
+        <img src="/mac.png" />
+      </div>
+      <h1 class="blink w-full">COOL STORY TYLER BUT NOW I'M SAD</h1>
+      <div>
+        <p>I was too! The past couple weeks I’ve been yelling to myself alone in the COVID Cave until I wanted to puke.</p>
+        <p>Instead, I'm going back to open source and opening an entry point for anyone trying hard and asking questions in this Brave Virtual World.</p>
+        <p>If you're trying to learn new skills to find a job, WordPress can help:</p>
+      </div>
+      <div>
+        <img id="usage" src="usage.png" />
+        <ul>
+        <li><strong>About a third of The Internet runs on WordPress.</strong> This means even just knowing how to use the /wp-admin/ interface in the back-end to update images, configure posts, export data, etc. could make you useful to a lot of people.</li>
+        <li>If you want to dig further and actually write code, WP offers a lovely balance of front-end and back-end work:</li>
+          <ul>
+            <li>CSS + Markup through <a href="https://codex.wordpress.org/Theme_Development" target="_blank">Themes in front</a></li>
+            <li>Object-Oriented Programming and MYSQL through <a href="https://codex.wordpress.org/Plugin_API" target="_blank">Plugins in the back.</a></li>
+            </ul>
+            </ul>
+              <p>If I had to design an intro curriculum to get people a web dev job ASAP, it would be two words: Learn WordPress. The cost of admission is $0, and classes start whenever you decide to commit and read the documentation. Speaking of which: there are <a href="https://codex.wordpress.org/Getting_Started_with_WordPress" target="_blank">tutorials</a>, <a href="https://www.youtube.com/results?search_query=wordpress+2020+getting+started" target="_blank">videos</a> and <a href="https://wordpress.stackexchange.com/" target="_blank">forums</a> <a href="https://old.reddit.com/r/Wordpress/" target="_blank">galore</a></p>
+              <p>Also a <a href="https://codex.wordpress.org/es:Main_Page" target="_blank">multi-lingual</a>, <a href="https://www.youtube.com/results?search_query=wordpress+tutorial+hindi" target="_blank">worldwide</a> community with (currently virtual) <a href="https://www.meetup.com/topics/wordpress/" target="_blank">meetups.</a> It is <strong>so easy</strong> to get help when learning WordPress and this is literally why The Internet was invented: to enable us to learn together.</p>
+      </div>
+      <div>
+        <p>If you are a small business, non-profit, or activist group looking to begin or update your digital presence, WordPress is the best option because it’s been used and tested by millions for the last 17 years and costs $0/month. You may still have to pay a developer for the labor of getting your templates and plugins configured, but there are <strong> no SaaS-y annual contracts or monthly fees to run the WP core.</strong> Lots of free or cheap plugins serving all common business needs, and at most your paying a WP dev an hourly rate for a few hours of their time, instead of spending weeks in meetings trying to scope a budget with an agency. Thanks to the recent <a href="https://developer.wordpress.org/block-editor/">Gutenberg</a> updates, along with the <a href="https://developer.wordpress.org/rest-api/">REST API</a>, and the awesome work by the contributors on <a href="https://github.com/jaredpalmer/presspack">PressPack</a> we can now use modern build tools like Node Package Manager, Webpack, and PostCSS to make WP as powerful an option as any other platform on the web today.</p>
+      </div>
+      <div>
+        <p>This is really important and we need to start right now. 
+          We need to start doing our own work and hosting our own content.</p>
+        <p>We need to stop numbly accepting that we give advertising and consumer dollars to US companies that cooperate with foreign leaders who committing real evil on a daily basis, and that the leaders of these companies are allowed to continue hyping their hyper-growth while people are kidnapped and tortured.</p>
+        <p>Imagine logging onto Instagram only there were no ads and all the content was original art and poetry and editorial commentary from weird internet people who have day-jobs and so sacrifice here, making zero dollars on their art and working out of passion and love. Imagine staring at something like this all the time, instead of corporate media. Do you think you'd feel different?</p>
+        <img class="mb-6" src="/nasdaq.png" />
+      </div>
+      <div>
+        <p>Please email me ASAP at tylerhillwebdev@gmail.com</p>
+      </div>
+      <div>
+        <p>Thank you for your time.</p>
+        <h1 class="blink">--:--:--</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import words from "~/assets/words.json";
+export default {
+  data() {
+    return {
+      words,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  background: cyan;
+  font-family: "vcr";
+}
+@font-face {
+  font-family: "vcr";
+  src: url("/vcr.ttf");
+}
+p {
+  @apply mb-6 text-lg;
+}
+ul {
+  list-style: circle;
+  padding-left: 20px;
+  @apply text-lg mb-6 py-4;
+  ul {
+    list-style: square;
+  }
+}
+.blink {
+  font-size: 60px;
+  color: blue;
+  @apply mx-auto py-8 w-full text-center;
+  animation: blink-animation 2s steps(5, start) infinite;
+  -webkit-animation: blink-animation 2s steps(5, start) infinite;
+}
+@keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+@-webkit-keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+li {
+  @apply mb-4 text-lg;
+}
+
+ol {
+  list-style: decimal;
+  color: blue;
+  @apply pl-12;
+}
+ul {
+  color: magenta;
+}
+a {
+  text-decoration: underline;
+  color: blue;
+  @apply p-2;
+  border: 2px solid #6ec36e;
+  white-space: nowrap;
+}
+</style>
---
+++ pages/rumi.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="flex flex-col justify-center items-center all">
+<p>Don't unstring the bow.</p>
+<p>I am your four-feathered arrow</p>
+<p>that has not been used yet.</p>
+<p>I am a strong knifeblade word,</p>
+<p>not some if or maybe, dissolving in air.</p>
+<p>I am sunlight slicing the dark.</p>
+<p>Who made this night?</p>
+<p>A forge deep in the earth-mud.</p>
+
+<p>What is the body? Endurance.</p>
+<p>What is love? Gratitude.</p>
+<p>What is hidden in our chests? Laughter.</p>
+<p>What else? Compassion.</p>
+
+<p>Let the beloved be a hat pulled down firmly on my head.</p>
+<p>Or drawstrings pulled and tied around my chest.</p>
+
+<p>Someone asks, How does love have hands and feet?</p>
+<p>Love is the sprouting bed for hands and feet!</p>
+<p>Your father and mother were playing love games.</p>
+<p>They came together, and you appeared!</p>
+
+<p>Do not ask what love can make or do!</p>
+<p>Look at the colors of the world.</p>
+<p>The riverwater moving in all rivers at once.</p>
+<p>The truth that lives in Shams' face.</p>
+</div>
+</template>
+<style>
+@font-face {
+  font-family: "vcr";
+  src: url("/vcr.ttf");
+}
+html, body {
+    font-family: "vcr";
+    background: black;
+    color: cyan;
+}
+.all {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
