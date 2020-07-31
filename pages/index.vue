bf488c1a497aa5cc3d84182b016e8ffa9f197e85
TheDripper <tylerehill@gmail.com>
2020-07-30 23:28:45 -0700

    predator

--- pages/index.vue
+++ pages/index.vue
@@ -126,6 +126,7 @@
         And for all you tie-wearing pundits out there brushing me off as another paranoid stoner, I implore you: why did <span class="magenta">The Washington Post,</span> a <span class="magenta">Bezos-owned</span> mouthpiece for the rich, which is currently blowing smoke in defense of facists like Trump:</p>
               <img src="/post_reddit_1.png" />
       <img src="/post_reddit_2.png" />
+      <p class="subnow py-16">Trump is a <span class="magenta">proven sexual predator</span> and you'd think The Washington Post <a href="https://en.wikipedia.org/wiki/Donald_Trump_Access_Hollywood_tape" target="_blank">would know this already</a> but I guess things have changed re: payroll in the last 4 years. </p>
       <a class="subnow mb-32" target="_blank" href="https://www.washingtonpost.com/opinions/2020/04/24/joe-biden-accused-trump-trying-delay-election-he-should-know-better/">Here is where Henry Olsen proves he's a shill for facism, and an idiot.</a>
         
         <p class="subnow magenta">Why did this same farce of a newspaper take the time and resources to drag Gary Webb's name in the dirt, even after he killed himself?</p>
