4294d31b87148718c60562a32d7ac2892cbf0ba9
TheDripper <tylerehill@gmail.com>
2020-08-07 10:46:15 -0700

    say their names

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="root">
     <div class="slide middle p-6">
-      <p
-        class="subnow black"
-      >I'm trying to get all the <span class="magenta">white, male managers/team-leads in VC-funded SF tech companies</span> to send their teams home and refuse to work until Gavin Newsom passes <span class="magenta">legislation creating a publicly-appointed CA-state Overseer/Auditor for Law Enforcement</span>, similar to OSHA/Food Safety inspectors, for all PD and Sheriff in California.</p>
-      <p class="subnow black"><span class="magenta">Work will remain halted for any company</span> with VC that can be traced to <span class="magenta">China, Russia, Poland, Brazil, or Saudi</span> until: the <span class="magenta">Uighur Muslims</span> are liberated, Andrzej Duda stops kidnapping <span class="magenta">LGBTQ</span>, and Bolsonaro stops committing Genocide of <span class="magenta">Indigenous Peoples</span> in the Amazon so he can burn it for beef.</p>
-      <p class="subnow">No more <span class="magenta">unicorns.</span></p>
-      <p class="subnow">Our <span class="magenta">house</span> is on <span class="magenta">fire.</span></p>
-      <p class="subnow">To learn more, search <span class="magenta">@hep_cat</span> on Twitter, or read this essay:</p>
-      <h1 class="subnow text-center">
+      <p class=" black">
+        I'm trying to get all the
+        <span
+          class="magenta"
+        >white, male managers/team-leads in VC-funded SF tech companies</span> to send their teams home and refuse to work until Gavin Newsom passes
+        <span
+          class="magenta"
+        >legislation creating a publicly-appointed CA-state Overseer/Auditor for Law Enforcement</span>, similar to OSHA/Food Safety inspectors, for all PD and Sheriff in California.
+      </p>
+      <p>
+        POC need <span class="magenta">real proof</span> from the <span class="magenta">white professional community</span> that
+        <span class="subnow magenta">we stand in solidarity with the BLM protests</span>, and we never stop thinking about <span class="magenta subnow">Oscar Grant, Michael Brown, Eric Garner, Sandra Bland, Walter Scott, Philando Castile, Nia Wilson,</span> and now <span class="magenta subnow">Breonna Taylor, George Floyd,</span> and the <span class="magenta">hundreds of sisters and brothers and cousins</span> that continue to be taken by violence, year after year, <span class="magenta">decade after decade</span>, without a headline or a protest.
+      </p>
+      <p class=" black">
+        <span class="magenta">Work will remain halted for any company</span> with VC that can be traced to
+        <span class="magenta">China, Russia, Poland, Brazil, or Saudi</span> until: the
+        <span class="magenta">Uighur Muslims</span> are liberated, Andrzej Duda stops kidnapping
+        <span class="magenta">LGBTQ</span>, and Bolsonaro stops committing Genocide of
+        <span class="magenta">Indigenous Peoples</span> in the Amazon so he can burn it for beef.
+      </p>
+      <p class="">
+        No more
+        <span class="magenta">unicorns.</span>
+      </p>
+      <p class="">
+        Our
+        <span class="magenta">house</span> is on
+        <span class="magenta">fire.</span>
+      </p>
+      <p class="">
+        To learn more, search
+        <span class="magenta">@hep_cat</span> on Twitter, or read this essay:
+      </p>
+      <h1 class=" text-center">
         <a
           href="/rip-bam-house-on-fire"
         >The World is Heartbroken: Venture Capital must put Profits On Hold until we Value Human Life Again</a>
