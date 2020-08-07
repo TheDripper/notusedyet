9d440dc9811cfcf4ea1c2808d52480020fc648d0
TheDripper <tylerehill@gmail.com>
2020-08-07 14:10:49 -0700

    begin bam edits

--- pages/save2.vue
+++ pages/save2.vue
@@ -1,18 +1,12 @@
 <template>
   <div dir="ltr" class="reader-article-content p-12">
     <p>I lived in Oakland for 10 years and about 1.5 of them were spent on Mead Ave. This was 2013, so the Bay Area Housing Crisis was seeing its first wave. I was getting freelance webdev jobs off craigslist by exaggerating my skills on Skype and then googling PHP syntax all night. I needed cheap rent.</p>
-    <p>
-      At this point I was a 21 year old white kid from Burlingame, so the first time I turned the corner and parked on Mead, my adrenaline was surging. There were 2 or 3 clusters of big, tall, serious-looking black men, sitting on their porch or listening to music by their car. I got out and started looking for the address I'd found on the internet, and within a couple seconds a woman behind me shouted
-      <em>Hey!</em>
-    </p>
+    <p>At this point I was a 21 year old white kid from Burlingame, so the first time I turned the corner and parked on Mead, my adrenaline was surging. There were 2 or 3 clusters of big, tall, serious-looking black men, sitting on their porch or listening to music by their car. I got out and started looking for the address I'd found on the internet, and within a couple seconds a woman behind me shouted <em>Hey!</em></p>
     <p>I jumped and turned around, certain that I'd pissed someone off and they wanted me to go back to my side of town.</p>
-    <p>
-      <em>You're gonna get a ticket if you park there, it's street sweeping today.</em> It was an older lady in the top window of the house I'd parked in front of. I noticed the other window had a painting in it: it was a surreal and beautiful portrait of a man with long dreads. I'd later learn she'd painted it, and it was of her friend.
-    </p>
+    <p><em>"You're gonna get a ticket if you park there, it's street sweeping today."</em> It was an older lady in the top window of the house I'd parked in front of. I noticed the other window had a painting in it: it was a surreal and beautiful portrait of a man with long dreads. I'd later learn she'd painted it, and it was of her friend.</p>
     <p>This experience of fear being met with kindness was repeated often as I came to become their neighbor. I came back that night with a bunch of boxes, it was raining, and the only parking spot was directly in front 5 big dudes, nothing but black clothes on, staring me down as I pulled up.</p>
     <p>
-      <em>Sorry if we're creepin' but we just gotta check out who you are...we all have adversaries, y'know?</em> One guy told me when I got out.
-      <em>You need help with these boxes?</em> The whole group started grabbing stuff and helped me and my girlfriend move it in. The first guy told me:
+      <em>"Sorry if we're creepin' but we just gotta check out who you are...we all have adversaries, y'know?"</em> One guy told me when I got out. "You need help with these boxes?" The whole group started grabbing stuff and helped me and my girlfriend move it in. The first guy told me:
       <em>don't leave anything in the car, even these empty boxes, cause people will bop the window even if it just looks like maybe something.</em> His name was Bam and he was living in a late 80s Chevy Cavalier in front of the corner store.
     </p>
     <p>
@@ -31,15 +25,14 @@
       <img
         alt="RIP BAM"
         data-media-urn
-        data-li-src="https://media-exp1.licdn.com/dms/image/C5612AQFz6CxfU4Kg7w/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=ui6WzG7472CgXi8QascH9EAaRFwqdU-ntyyqFK4pmog"
-        src="https://media-exp1.licdn.com/dms/image/C5612AQFz6CxfU4Kg7w/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=ui6WzG7472CgXi8QascH9EAaRFwqdU-ntyyqFK4pmog"
+        data-li-src="/rip_bam.png"
+        src="/rip_bam.png"
       />
     </div>
     <p>
       <br />
     </p>
-    <p>There's a poster in the window of the apartment across the street from mine:</p>
-    <h3>BLACK VOTES MATTER.</h3>
+    <p>There's a poster in the window of the apartment across the street from mine: BLACK VOTES MATTER.</p>
     <p>
       <a
         href="https://www.cambridge.org/core/journals/perspectives-on-politics/article/testing-theories-of-american-politics-elites-interest-groups-and-average-citizens/62327F513959D0A304D4893B382B992B"
@@ -50,9 +43,7 @@
     <div class="slate-resizable-image-embed slate-image-embed__resize-full-width">
       <img
         alt="No alt text provided for this image"
-        data-media-urn
-        data-li-src="https://media-exp1.licdn.com/dms/image/C5612AQEsibrR-3Ifpg/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=5_VqRBcxrmj_NUxK8vKuwM_gVBWKxuvPjOSxsQPx_Aw"
-        src="https://media-exp1.licdn.com/dms/image/C5612AQEsibrR-3Ifpg/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=5_VqRBcxrmj_NUxK8vKuwM_gVBWKxuvPjOSxsQPx_Aw"
+        src="/princeton_voter_study_2014.png"
       />
     </div>
     <p>
@@ -92,22 +83,11 @@
       >
         <em>operating a VC-funded startup fronting as Californian but is actually based in Russia</em>
       </a>
-      <em>is probably cooperating with the FSB, no? Very hard to argue this money doesn't eventually end up buying</em>
-      <strong>
-        <em>Barrel Bombs for Aleppo</em>
-      </strong>
-      <em>.</em>
-    </p>
-    <p>
-      <em>$50M in funding for</em>
+      <em>is probably cooperating with the FSB, no? $50M in funding for</em>
       <strong>
         <em>another SaaS eCommerce solution</em>
       </strong>
-      <em>that provides "Online selling solutions for small businesses"?</em>
-      <strong>
-        <em>Sounds super legit</em>
-      </strong>
-      <em>. Even his</em>
+      <em>that provides "Online selling solutions for small businesses"? Sounds super legit. Even his</em>
       <a
         href="https://en.wikipedia.org/wiki/Ruslan_Fazlyev"
         target="_blank"
@@ -133,7 +113,8 @@
         alt="No alt text provided for this image"
         data-media-urn
         data-li-src="https://media-exp1.licdn.com/dms/image/C5612AQGXqPP3D0znlA/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=Of47WO5DxT62pUgFR4HATBgIrb5BqPjb_-HdeNj4FE4"
-        src="https://media-exp1.licdn.com/dms/image/C5612AQGXqPP3D0znlA/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=Of47WO5DxT62pUgFR4HATBgIrb5BqPjb_-HdeNj4FE4"
+        src="/salwa_laughs_at_bombs.png"
+
       />
     </div>
     <p>
@@ -150,24 +131,25 @@
       This farce of concern from VC Unicorns is one of the reasons I'm so bitter and high-strung all the time. I've actually lost friendships because people get tired of hearing me rant. I'll hear McConnell say about 5 words and I'm tweaking the whole night; I can
       <em>feel</em>that voice. This is not just some Story on the Feed for me. It is a constant droning agony in my heart that makes me want to punch windows.
     </p>
-    <p>We had shootings a lot on Mead, usually nobody hits anything. But when they do, you can tell: everything is totally still and silent, because whoever was outside is dead. This lasts for a few seconds before the tires peel out, then each porches' front door opening and each neighbor looking down to see the 2 bodies on the corner by the Boys &amp; Girls club, totally still. We are making an excruciating and familiar procession to keep watch until OPD throws up the tape.</p>
+    <p>We had shootings a lot on Mead, usually nobody hits anything. But when they do, you can tell: everything is totally still and silent, because whoever was outside is dead. This lasts for a few seconds before the tires peel out, then each porches' front door opening and each neighbor looking down to see the 2 bodies on the corner by the Boys &amp; Girls club, totally still. We are making a heartbreaking but somehow familiar procession to keep watch until OPD throws up the tape.</p>
     <p>This might have been just another bad night, blurred impossibly with all the others.</p>
     <p>Coming the other way is a kid, maybe 13, slick with tears and sobbing as he states very plainly: "Now I gotta go tell my mama my big brother is dead."</p>
     <p>A few minutes later we hear her screaming the way only a mother who has just lost her young son can. The kid has stopped crying and is acting full man now, before he can drive, taking her by the arm and telling her it's ok while they go to ID the body. She keeps just keeps saying "no no no no no no no no" until she sees the body, then it's a sound I can't type.</p>
     <p>I don't think about this often because I can't. This was by far the worst night, but things like this happened so often in Oakland that I actually watched the adrenaline response in my body adapt to gunshots: at first they made my heart race, by the end I often didn't notice until someone else mentioned it.</p>
-    <p>What's most memorable about Mead Ave is that despite this, it was the best place I've ever lived. I knew every single neighbor on the block because they'd each taken the time to come over and introduce themselves. I never worried about anything happening to my car or house, because my neighbors were always around keeping an eye out. Raymond next-door was a DJ who played everything from Marvin Gaye to Michael Jackson to Biggie. On Friday night, we'd just wander between peoples porches and yards and play music from the car. This is business as usual in their community, but for me after 18 years in a suburb where everybody is hidden quietly away behind ornate fencing, it was totally magical and insane.</p>
+    <p>What's most memorable about Mead Ave is that despite this, it's the best place I've ever lived. I knew every single neighbor on the block because they'd each taken the time to come over and introduce themselves. I never worried about anything happening to my car or house, because my neighbors were always around keeping an eye out. Raymond next-door was a DJ who played everything from Marvin Gaye to Michael Jackson to Biggie. On Friday night, we'd just wander between peoples porches and yards and play music from the car. This is business as usual in their community, but for me after 18 years in a suburb where everybody is hidden quietly away behind ornate fencing, it was totally magical and insane.</p>
     <div class="slate-resizable-image-embed slate-image-embed__resize-full-width">
       <img
         alt="RICH AF"
+        class="mb-16"
         data-media-urn
         data-li-src="https://media-exp1.licdn.com/dms/image/C5612AQHlYgV23bvQOQ/article-inline_image-shrink_1500_2232/0?e=1602115200&amp;v=beta&amp;t=2qOzNYulRzYYA4Klek3hrHeP5WVhgTHHkJ0MOPcadik"
-        src="https://media-exp1.licdn.com/dms/image/C5612AQHlYgV23bvQOQ/article-inline_image-shrink_1500_2232/0?e=1602115200&amp;v=beta&amp;t=2qOzNYulRzYYA4Klek3hrHeP5WVhgTHHkJ0MOPcadik"
+        src="/white_rich_people.png"
       />
     </div>
     <p>White professionals in Silicon Valley need to stop enabling these smiling VC thespians by pretending this economy isn't absolutely sick. Before COVID hit I was working at startup that provided dinner, and every night POC twice my age would haul big heavy racks of food up the elevator and serve me. White Liberals don't really have radar for when everybody light-skinned in the room is in a nice hip outfit and anybody a shade too dark is in a uniform with the logo of the company they work for. Whenever I'd make a comment about this I was met with total shock and silence. There was usually some appeal like "Well I'm glad for the thing Executives/Leadership said about it today at the town hall."</p>
     <p>
       What
-      <em>did</em>ELT say about BLM? This is not verbatim, but at that company, it was to the effect of: "Look, this isn't even about politics. It's just good business. Our customers want it, the team wants it, it's a win-win-win."
+      <em>did</em> ELT say about BLM? This is not verbatim, but at that company, it was to the effect of: "Look, this isn't even about politics. It's just good business. Our customers want it, the team wants it, it's a win-win-win."
     </p>
     <p>
       This is VCs answer to BLM:
@@ -184,19 +166,11 @@
       <strong>
         <em>Religious Minorities</em>
       </strong>
-      <em>are the</em>
-      <strong>
-        <em>antithesis of our country's battle cry</em>
-      </strong>
-      <em>, but why</em>
+      <em>are basically the antithesis of the USA spirit, but why</em>
       <strong>
         <em>shouldn't</em>
       </strong>
-      <em>we still make</em>
-      <strong>
-        <em>billions of USD per quarter</em>
-      </strong>
-      <em>?</em>
+      <em>we still make billions of USD per quarter?</em>
     </p>
     <p>
       Meanwhile people like Sheryl Sandberg,
@@ -206,26 +180,20 @@
         rel="nofollow noopener"
       >who please recall sold the 2016 election to Putin to maintain #hypergrowth and is a big part of why Trump is president</a>, are struggling to read cue-cards about resilience from underneath the pile of money that is falling from the sky.
     </p>
-    <p>I am 100% deadass about the following.</p>
-    <h2>
-      <strong>posters &amp;&amp; #hashtags != solidarity.</strong>
-    </h2>
-    <h2>White male software engineers in Silicon Valley must strike now for black lives.</h2>
     <p>
-      <strong>No more OKRs, no more Zoom/Slack/Jira/Asana/Airtable/Workday/Carta.</strong>Tell anyone working around you that
-      <strong>Black Lives Matter</strong>and to
-      <strong>log out of work</strong> until Gavin Newsom and CA Assembly/Senate pass legislation creating a
+      I am 100% deadass about the following.</p>
+      <p>
+      if you are a
+      <strong>white male who leads a team for a VC-funded company in Silicon Valley</strong>, tell ELT/Founders/Angels/etc:
+      <strong>no more OKRs, no more Zoom or Slack or Jira.</strong>Tell anyone working under you that
+      <strong>Black Lives Matter</strong>, then tell them to
+      <strong>turn their laptops off</strong> until Gavin Newsom and CA Assembly/Senate pass legislation creating a
       <strong>publicly-appointed Overseer/Auditor for Law Enforcement</strong>, similar to OSHA/Food Safety inspectors. All PD / Sheriffs in the State of CA would be required to submit an annual report, covering shooting deaths, excessive force complaints, etc. The Overseer and a small staff would then
       <strong>audit these reports and surface any issues</strong> to the Assembly, the Senate, and the Governor.
     </p>
     <p>
-      If you are a manager, tell your team to direct any questions from leadership to you, and stop assigning tasks. Make sure ELT knows you are a true leader, and that
+      Tell your team to direct any questions from leadership to you, and stop assigning tasks. Make sure ELT knows you are a true leader, and that
       <strong>their beef is with you.</strong>
-    </p>
-    <p>
-      Millions of people around
-      <strong>the world will</strong>
-      <strong>have your back.</strong>
     </p>
     <p>
       POC need
@@ -235,96 +203,42 @@
       <strong>Breonna Taylor, George Floyd</strong>, and the hundreds of
       <strong>sisters</strong> and
       <strong>brothers</strong> and
-      <strong>cousins</strong> that continue to be taken by violence year after year,
-      <strong>decade after decade,</strong> without a headline or a protest.
+      <strong>cousins</strong> that continue to be taken by violence every year without a headline or a protest.
     </p>
     <p>
-      <strong>Work will also remain halted for any company that can be traced to Venture Capital in:</strong>
+      <strong>Work will also remain halted for any company that can be traced to Venture Capital in China / Russia / Saudi / Brazil / Poland, period. No whining that you're "totally uninvolved", business leaders in all countries need to quit making billion dollar deals. Money will not flow as long as</strong>
     </p>
     <ul>
       <li>
-        <strong>China</strong>
-      </li>
-      <li>
-        <strong>Russia</strong>
-      </li>
-      <li>
-        <strong>Poland</strong>
-      </li>
-      <li>
-        <strong>Brazil</strong>
-      </li>
-      <li>
-        <strong>Saudi</strong>
-      </li>
-    </ul>
-    <h3>
-      <strong>UNTIL</strong>
-    </h3>
-    <ul>
-      <li>
-        <strong>Uighur Muslims in Xinjiang</strong>
-      </li>
-      <li>
-        <strong>Political dissidents in Hong Kong</strong>
-      </li>
-      <li>
-        <strong>LGBTQ in Poland</strong>
+        <strong>Muslims in Xinjiang</strong>
       </li>
       <li>
         <strong>Central/South American refugees at the Southern US Border</strong>
       </li>
       <li>
+        <strong>Political Dissidents under Putin, Jingping, MBS, and al-Assad</strong>
+      </li>
+      <li>
         <strong>Indigenous Peoples in Australia, Canada, Brazil and the US</strong>
       </li>
       <li>
-        <strong>Journalists, Critics, Medics, and Non-violent Protestors against:</strong>
+        <strong>LGBTQ in Poland under Andrzej Duda</strong>
       </li>
     </ul>
-    <h3>
-      <strong>=&gt; Xi Jingping</strong>
-    </h3>
-    <h3>
-      <strong>=&gt; Andrzej Duda</strong>
-    </h3>
-    <h3>
-      <strong>=&gt; Jair Bolsonaro</strong>
-    </h3>
-    <h3>
-      <strong>=&gt; MBS</strong>
-    </h3>
-    <h3>
-      <strong>=&gt; al-Assad</strong>
-    </h3>
-    <h3>
-      <strong>=&gt; Vladimir Putin</strong>
-    </h3>
-    <h3>=&gt; Rodrigo Duterte</h3>
-    <h3>
-      <strong>are no longer kidnapped, tortured, imprisoned, murdered, and disappeared.</strong>
-    </h3>
     <p>
-      <strong>SaaS</strong> / WWW products are the
-      <strong>ideal target for a strike</strong> because:
+      <strong>are no longer kidnapped, tortured, imprisoned, murdered, and disappeared.</strong>
     </p>
+    <p>SaaS / WWW products are the ideal target for a strike because:</p>
     <ul>
       <li>
         unlike other investments that require large factories for storage or processing,
         <em>e.g. food distribution or scientific research</em>,
         <strong>Slack / Jira / Instagram</strong>etc. suffer no damage from being turned off for a few months.
       </li>
+      <li>The massive concentration of capital in these companies has allowed them to continue profiting while the majority of us on earth are struggling to breathe in the most real sense.</li>
       <li>
-        The massive concentration of capital in these companies has allowed them to
-        <strong>step over this crisis</strong> and
-        <strong>maintain record growth</strong> while the majority of us on earth are
-        <strong>struggling to breathe</strong> in the most real sense.
-      </li>
-      <li>
-        <strong>We control this capital</strong>. The second we
-        <strong>take our eyes off their ads and stop writing code</strong>, their money
-        <strong>vanishes</strong>. The only damage will be to the
-        <strong>growth forecasts of early-seed white men</strong> in SF and their
-        <strong>international partners</strong>.
+        We control this capital. The second we
+        <strong>take our eyes off their ads and stop writing code</strong>, their money vanishes. The only damage will be to the growth forecasts of early-seed white men in SF and their international partners.
       </li>
     </ul>
     <h2>
@@ -335,7 +249,7 @@
         alt="No alt text provided for this image"
         data-media-urn
         data-li-src="https://media-exp1.licdn.com/dms/image/C5612AQGZX1LaVqWaMg/article-inline_image-shrink_1000_1488/0?e=1602115200&amp;v=beta&amp;t=Tk0ei5xFWjeV1YmhymNEyNXICzPHdiHTlntDWapczqw"
-        src="//:0"
+        src="/nasdaq_fam.png"
       />
     </div>
     <iframe
