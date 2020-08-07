ed60c32246a2fa141e3386cdf865b391a1e2cd65
TheDripper <tylerehill@gmail.com>
2020-08-07 10:30:47 -0700

    no more unicorns

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,9 @@
         class="subnow black"
       >I'm trying to get all the <span class="magenta">white, male managers/team-leads in VC-funded SF tech companies</span> to send their teams home and refuse to work until Gavin Newsom passes <span class="magenta">legislation creating a publicly-appointed CA-state Overseer/Auditor for Law Enforcement</span>, similar to OSHA/Food Safety inspectors, for all PD and Sheriff in California.</p>
       <p class="subnow black"><span class="magenta">Work will remain halted for any company</span> with VC that can be traced to <span class="magenta">China, Russia, Poland, Brazil, or Saudi</span> until: the <span class="magenta">Uighur Muslims</span> are liberated, Andrzej Duda stops kidnapping <span class="magenta">LGBTQ</span>, and Bolsonaro stops committing Genocide of <span class="magenta">Indigenous Peoples</span> in the Amazon so he can burn it for beef.</p>
-      <p class="subnow magenta">To learn more, search @hep_cat on Twitter, or read this essay:</p>
+      <p class="subnow">No more <span class="magenta">unicorns.</span></p>
+      <p class="subnow">Our <span class="magenta">house</span> is on <span class="magenta">fire.</span></p>
+      <p class="subnow">To learn more, search <span class="magenta">@hep_cat</span> on Twitter, or read this essay:</p>
       <h1 class="subnow text-center">
         <a
           href="/rip-bam-house-on-fire"
