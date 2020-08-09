188bffa5c8869ea8fbb69e2fa86a5aad80b8a0fa
TheDripper <tylerehill@gmail.com>
2020-08-09 13:22:51 -0700

    pick up that can

---
+++ pages/can.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="root">
+    <img src="/pick_up_that_can_og.png" />
+  </div>
+</template>
+<style scoped lang="scss">
+img {
+  position: fixed;
+  left: 50vw;
+  top: 50vh;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ pages/can_press.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="root">
+    <img src="/pick_up_that_can.jpg" />
+  </div>
+</template>
+<style scoped lang="scss">
+img {
+  position: fixed;
+  left: 50vw;
+  top: 50vh;
+  transform: translate(-50%, -50%);
+  filter: saturate(500%);
+}
+</style>
