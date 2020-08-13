79b9042820fa5d8536ed346bd771641f14e1ac87
TheDripper <tylerehill@gmail.com>
2020-08-12 21:04:49 -0700

    clip intro

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,71 @@
 <template>
   <div id="root">
     <div class="slide p-1">
-      <h3 class="shadow uppercase blink">likes &amp;&amp; <span class="black">#</span>hashtags <span class="black">!</span>= solidarity.</h3>
-    <h3 class="shadow uppercase blink">likes &amp;&amp; <span class="black">#</span>hashtags <span class="black">!</span>= solidarity.</h3>
-    <h1 class="shadow"><span class="black">White men in Silicon Valley must</span> <span class="uppercase">Occupy Venture Capital</span> <span class="black"> and</span></h1>
-    <h1 class="shadow"><span class="uppercase">STRIKE NOW</span> <span class="black">FOR</span> BLACK LIVES<span class="black blink">!</span></h1>
-    <h1 class="shadow"><span class="uppercase">STRIKE NOW</span> <span class="black">FOR</span> HONG KONG!<span class="black blink">!</span></h1>
-    <h1 class="shadow"><span class="uppercase">STRIKE NOW</span> <span class="black">FOR</span> UIGHUR MUSLIMS!<span class="black blink">!</span></h1>
-    <h1 class="shadow"><span class="uppercase">STRIKE NOW</span> <span class="black">FOR</span> THE AMAZON!<span class="black blink">!</span></h1>
-    <ul id="why_white_men_need_to_occupy_venture_capital">
-      <li><h3 class="shadow"><span class="black ">i am a white man and <a href="https://www.google.com/search?q=average+salary+front+end+engineer&rlz=1C5CHFA_enUS847US847&oq=average+salary+front+end+engineer&aqs=chrome.0.69i59l2j0l2.5466j0j7&sourceid=chrome&ie=UTF-8" target="_blank">i write code for $$$</a> in SF</span></h3></li>
-      <div class="relative">
-        <img src="/tyler_hill_white_kid_check.jpg" />
-        <img id="dayjob" src="/dayjob.jpg" />
-        <img src="/tyler_hill_lives_here.jpg" />
-        <img src="/tyler_hill_real_person.jpg" />
+      <div class="slide flex staticbg items-center justify-center">
+        <div id="sean_monterrosa" class="relative frame">
+          <img v-for="src in black" :src="src" />
+        </div>
+        <div id="agnes_chow" class="relative frame">
+          <img 4 v-for="src in white" :src="src" />
+        </div>
+        <div id="strike_now_occupy_venture_capital">
+          <h3 class="shadow uppercase blink">
+            likes &amp;&amp;
+            <span class="black">#</span>hashtags
+            <span class="black">!</span>= solidarity.
+          </h3>
+          <h3 class="shadow uppercase blink">
+            likes &amp;&amp;
+            <span class="black">#</span>hashtags
+            <span class="black">!</span>= solidarity.
+          </h3>
+        </div>
       </div>
-    </ul>
+
+      <h1 class="shadow">
+        <span class="black">White men in Silicon Valley must</span>
+        <span class="uppercase">Occupy Venture Capital</span>
+        <span class="black">and</span>
+      </h1>
+      <h1 class="shadow">
+        <span class="uppercase">STRIKE NOW</span>
+        <span class="black">FOR</span> BLACK LIVES
+        <span class="black blink">!</span>
+      </h1>
+      <h1 class="shadow">
+        <span class="uppercase">STRIKE NOW</span>
+        <span class="black">FOR</span> HONG KONG!
+        <span class="black blink">!</span>
+      </h1>
+      <h1 class="shadow">
+        <span class="uppercase">STRIKE NOW</span>
+        <span class="black">FOR</span> UIGHUR MUSLIMS!
+        <span class="black blink">!</span>
+      </h1>
+      <h1 class="shadow">
+        <span class="uppercase">STRIKE NOW</span>
+        <span class="black">FOR</span> THE AMAZON!
+        <span class="black blink">!</span>
+      </h1>
+      <ul id="why_white_men_need_to_occupy_venture_capital">
+        <li>
+          <h3 class="shadow">
+            <span class="black">
+              i am a white man and
+              <a
+                href="https://www.google.com/search?q=average+salary+front+end+engineer&rlz=1C5CHFA_enUS847US847&oq=average+salary+front+end+engineer&aqs=chrome.0.69i59l2j0l2.5466j0j7&sourceid=chrome&ie=UTF-8"
+                target="_blank"
+              >i write code for $$$</a> in SF
+            </span>
+          </h3>
+        </li>
+        <div class="relative">
+          <img src="/tyler_hill_white_kid_check.jpg" />
+          <img id="dayjob" src="/dayjob.jpg" />
+          <img src="/tyler_hill_lives_here.jpg" />
+          <img src="/tyler_hill_real_person.jpg" />
+        </div>
+      </ul>
       <p>
         No more
         <a
@@ -74,11 +123,17 @@
           have your back.
         </span>
       </p>
-      <img id="verify_facts_help_others" src="/verify_facts_help_others_clip.gif" />
-      <img src="/nasdaq_fam_tokenism.jpg" />
       <div id="poc_need_proof">
-        <h3 class="black shadow uppercase">STRIKE NOW AGAINST THE FARCE OF DIVERSITY FROM EARLY-SEED OLIGARCHS!</h3>
-        <h3 class="shadow uppercase">STRIKE NOW AGAINST THE <a href="https://www.google.com/search?q=relentless+dot+com&rlz=1C5CHFA_enUS847US847&oq=relentless+dot+com&aqs=chrome..69i57j0l4j5.11440j1j7&sourceid=chrome&ie=UTF-8" target="_blank">RELENTLESS</a> CONCENTRATION OF WEALTH BY WHITE MEN!</h3>
+        <h3
+          class="black shadow uppercase"
+        >STRIKE NOW AGAINST THE FARCE OF DIVERSITY FROM EARLY-SEED OLIGARCHS!</h3>
+        <h3 class="shadow uppercase">
+          STRIKE NOW AGAINST THE
+          <a
+            href="https://www.google.com/search?q=relentless+dot+com&rlz=1C5CHFA_enUS847US847&oq=relentless+dot+com&aqs=chrome..69i57j0l4j5.11440j1j7&sourceid=chrome&ie=UTF-8"
+            target="_blank"
+          >RELENTLESS</a> CONCENTRATION OF WEALTH BY WHITE MEN!
+        </h3>
         <h3 class="black shadow uppercase">STRIKE NOW TO SHOW POC IN THE USA:</h3>
         <h3 class="blink shadow uppercase">WE ARE DIFFERENT!</h3>
         <span class="bump">
@@ -176,7 +231,10 @@
         <ul id="actual_patriarchy">
           <li>
             <h3 class="shadow uppercase">
-              <a href="https://www.google.com/search?rlz=1C5CHFA_enUS847US847&q=Alexander+Lukashenko&stick=H4sIAAAAAAAAAONgVuLUz9U3MDTJTjJ6xGjCLfDyxz1hKe1Ja05eY1Tl4grOyC93zSvJLKkUEudig7J4pbi5ELp4FrGKOOakViTmpaQWKfiUZicWZ6TmZecDAHwIV1BbAAAA&biw=1745&bih=889" target="_blank">Alexander Lukashenko</a>
+              <a
+                href="https://www.google.com/search?rlz=1C5CHFA_enUS847US847&q=Alexander+Lukashenko&stick=H4sIAAAAAAAAAONgVuLUz9U3MDTJTjJ6xGjCLfDyxz1hKe1Ja05eY1Tl4grOyC93zSvJLKkUEudig7J4pbi5ELp4FrGKOOakViTmpaQWKfiUZicWZ6TmZecDAHwIV1BbAAAA&biw=1745&bih=889"
+                target="_blank"
+              >Alexander Lukashenko</a>
             </h3>
           </li>
           <li>
@@ -377,6 +435,45 @@
   </div>
 </template>
 <style lang="scss" scoped>
+.frame {
+  height: 600px;
+  width: 600px;
+  overflow: hidden;
+  padding: 7px;
+  @apply flex items-center justify-center;
+}
+.copy {
+  @apply absolute;
+}
+#sean_monterrosa {
+  background: magenta;
+  .copy {
+    bottom: 270px;
+    left: 0;
+  }
+  img {
+    @apply absolute;
+    top: 0;
+    left: 0;
+  }
+}
+#agnes_chow {
+  background: cyan;
+  margin-left: 20px;
+  .copy {
+    margin-top: 20px;
+  }
+  img {
+    @apply absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    max-width: none;
+  }
+}
+.staticbg {
+  background-image: url("/static.jpg");
+}
 #occupy_venture_capital {
   color: black;
   border: none !important;
@@ -517,4 +614,92 @@ li {
   width: 100%;
   max-width: 800px;
 }
+#strike_now_occupy_venture_capital {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
 </style>
+<script>
+export default {
+  mounted() {
+    let black = this.black;
+    for (let i = 0; i < black.length; i++) {
+      setTimeout(function () {
+        black.pop();
+      }, i * 1000);
+    }
+    let white = this.white;
+    for (let i = 0; i < white.length; i++) {
+      setTimeout(function () {
+        white.pop();
+      }, i * 1000);
+    }
+  },
+  data() {
+    return {
+      black: [
+        "/shooting_of_larry_jackson_jr.png",
+        "/shooting_of_anthony_lamar_smith.png",
+        "/shooting_of_trayvon_martin.png",
+        "/shooting_of_mark_duggan.png",
+        "/shooting_of_ramarley_graham.png",
+        "/death_of_kelly_thomas.png",
+        "/shooting_of_anthony_hill.png",
+        "/shooting_of_timothy_russell_and_malissa_williams.png",
+        "/shooting_of_jonathan_ferrell.png",
+        "/shooting_of_john_crawford_iii.png",
+        "/shooting_of_terence_crutcher.png",
+        "/shooting_of_amadou_diallo.png",
+        "/shooting_of_samuel_dubose.png",
+        "/death_of_freddie_gray.png",
+        "/shooting_of_rekia_boyd.png",
+        "/shooting_of_tamir_rice.png",
+        "/shooting_of_sean_bell.png",
+        "/shooting_of_oscar_grant.png",
+        "/shooting_of_michael_brown.png",
+        "/death_of_eric_garner.png",
+        "/death_of_sandra_bland.png",
+        "/shooting_of_walter_scott.png",
+        "/shooting_of_philando_castile.png",
+        "/shooting_of_nia_wilson.png",
+        "/shooting_of_breonna_taylor.png",
+        "/killing_of_george_floyd.png",
+        "/shooting_of_sean_monterrosa.png",
+      ],
+      white: [
+        "/nike_of_the_mind.png",
+        "/nasdaq_fam",
+        "/we_will_stop_this_now.png",
+        "/we_need_to_stop.png",
+        "/us_russia_tech_deals.png",
+        "/unicorns_such_rarity.png",
+        "/salesforce_cartoon_origami_asmr.png",
+        "/saas.png",
+        "/ruslan_fazlyev_oligarch_putin.png",
+        "/nasdaq_human_heartbeat.png",
+        "/musk_phone.png",
+        "/market_share.png",
+        "/elon_pulls_out_california.png",
+        "/nasdaq_brp.png",
+        "/nasdaq_adaptive.png",
+        "/nasdaq_argenx.png",
+        "/nasdaq_docusign.png",
+        "/nasdaq_funko.png",
+        "/nasdaq_tillray.png",
+        "/nasdaq_can.png",
+        "/nasdaq_newtek.png",
+        "/nasdaq_blackbaud.png",
+        "/nasdaq_fluent.png",
+        "/nasdaq_pagerduty.png",
+        "/nasdaq_lyft.png",
+        "/nasdaq_ruhnn.png",
+        "/nasdaq_niu.png",
+        "/nasdaq_longfin.png",
+        "/nasdaq_paypal.png",
+      ],
+    };
+  },
+};
+</script>
